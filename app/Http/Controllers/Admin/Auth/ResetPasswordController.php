eb86a7bc43355fbf0cfbbde629b42ecb411f5d16
Aleksandar Mitic <aleksandar.webart@gmail.com>
2019-11-26 11:43:11 +0100

    Admin reset password finished

--- app/Http/Controllers/Admin/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Admin/Auth/ResetPasswordController.php
@@ -2,10 +2,69 @@
 
 namespace App\Http\Controllers\Admin\Auth;
 
-use App\Http\Controllers\Controller;
+use Auth;
+use Password;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
 {
-    //
+    /**
+     * This will do all the heavy lifting
+     * for resetting the password.
+     */
+    use ResetsPasswords;
+
+     /**
+     * Where to redirect users after resetting their password.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/admin/dashboard';
+
+    /**
+     * Only guests for "admin" guard are allowed except
+     * for logout.
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest:admin');
+    }
+
+    /**
+     * Display the password reset view for the given token.
+     *
+     * If no token is present, display the link request form.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string|null  $token
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showResetForm(Request $request, $token = null)
+    {
+        return view('admin.passwords.reset')->with(
+            ['token' => $token, 'email' => $request->email]
+        );
+    }
+
+    /**
+     * Get the broker to be used during password reset.
+     *
+     * @return \Illuminate\Contracts\Auth\PasswordBroker
+     */
+    protected function broker(){
+        return Password::broker('admins');
+    }
+    /**
+     * Get the guard to be used during password reset.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard(){
+        return Auth::guard('admin');
+    }
+
 }
